021dc0cf46eb7caee076ea337fcaf2e1f38f4403
gzenner <gzenner@student.42luxembourg.lu>
2025-11-18 15:36:05 +0100

    new_tasks

--- index.py
+++ index.py
@@ -23,24 +23,24 @@ def ReadSubjectFile(subject, level):
     return st.session_state['all_files'][subject][f'level{level}']
 
 def CleanTasks(all_level_tasks):
-    if all_level_tasks == -1: return -1
-    return [level for level in all_level_tasks if len(level) and 'END' not in level]
+    if all_level_tasks == None: return None
+    return ''.join(all_level_tasks).split('**=END=**')
 
 def XTasks(tasks, x):
     if x <= len(tasks): return r.sample(tasks, x)
     return r.sample(tasks, len(tasks))
 
 def PrintTasks(subjects):
-    level = str(st.slider('Waehle das Level aus:', 1, 10))
-    amount = st.slider('Waehle wieviele Aufgaben:', 1, 30)
+    level = str(st.slider('Waehle das Level aus:', 1, 3))
+    amount = st.slider('Waehle wieviele Aufgaben:', 1, 10)
     subject = st.selectbox(label="Waehle das Fach: ", options=subjects, index=0)
     Tasks = CleanTasks(ReadSubjectFile(subject, level))
-    if Tasks == -1: st.error("File not found"); exit()
+    if Tasks == None: st.error("File not found"); exit()
     tasks = XTasks(Tasks, amount)
     for task in tasks: st.write(f'{task}')
 
 def greet(name):
-    st.header(f"Welcome {name} to your Dynamic Homework Generator:\n")
+    st.header(f"Welcome {name} to your Dynamic Teaching Generator:\n")
 
 def run(name, level, subjects):
     s1 = Student(name, level, subjects)
